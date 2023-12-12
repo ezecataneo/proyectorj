dad9163c37b2bdf157d179deba5a2f8a4c76140d
Ezequiel Cataneo <ezecataneo1991@gmail.com>
2023-12-12 18:56:12 -0300

    se realiza carga asincrona en item list conteiner

--- src/components/Itemlistconteiners/ItemListConteiners.jsx
+++ src/components/Itemlistconteiners/ItemListConteiners.jsx
@@ -3,42 +3,82 @@ import {useState, useEffect} from "react"
 const MOCK_DATA = [
     {
         id: 1,
-        name: 'producto 1',
-        img : "",
+        name: 'TV SAMSUNG',
+        img : "/images/samsung-43.jpg",
         precio: 270.000,
         description : 'SMART LED TV SAMSUNG 43 PULGADAS FULL HD UN43T5300AGCZB'
-    }
-    
+    },
+
+    {
+        id: 2,
+        name: 'NOTEBOOK ASSUS',
+        img : "/images/asuss.webp",
+        precio: 374.999,
+        description : 'Notebook. Pantalla de 14" HD (1366 x 768) 16:9. Tasa de refresco 60 Hz. Brillo 200 nits. Procesador Intel Celeron N4020 Processor 1.1 GHz (4M Cache, up to 2.8 GHz, 2 cores). 128G eMMC. RAM 4GB DDR4 on board. Placa de video Intel UHD Graphics 600. Conexiones: Wi-Fi 5(802.11ac) (Dual band) 1*1 + Bluetooth 5.1 Wireless Card. 1x USB 2.0 Type-A. 1x USB 3.2 Gen 1 Type-A. 1x USB 3.2 Gen 1 Type-C. 1x HDMI 1.4. 1x Headphone/Headset. Microfono incorporado. Sistema operativo Windows 11 home. Camara VGA. Bateria 42WHrs, 3S1P, 3-cell Li-ion.'
+    },
+
+    {
+        id: 3,
+        name: 'CELULAR MOTOROLA',
+        img : "/images/celumoto.webp",
+        precio:  241.499,
+        description : 'Alto: 161.78.  Ancho: 73.84.  Color: PLATA SATINADO.  Memoria RAM: 6 GB.  Bluetooth: SI.  Camara principal: 50 MP + 8 MP + 2 MP.'
+    },
+
+    {
+        id: 4,
+        name: 'CELULAR SAMSUNG',
+        img : "/images/celusamsung.webp",
+        precio: 528.999,
+        description : 'Alto: 15.82.  Ancho: 7.67.  Color: GRAPHITE.  Memoria RAM: 8 GB.  Bluetooth: SI.  Camara principal: 50 + 12 + 5 MP.'
+    },
+
+    {
+        id: 5,
+        name: 'TV PHILIPS',
+        img : "/images/tvphilips.webp",
+        precio: 179.982,
+        description : 'SMART LED GOOGLE TV PHILIPS 32 PULGADAS HD 32PHD6918/77'
+    },
+
 ]  
+
+
+const pedirDatos = () => {
+    return new Promise ((resolve,reject) => {
+        setTimeout (() => {
+            resolve (MOCK_DATA)
+        },3000)
+})
+
+}
 
 
 export const ItemListConteiners = ({props}) => {
 
     const [productos, setProductos] = useState([])
 
-    
-
-    const pedirDatos = () => {
-        return promesa = new Promise ((resolve,reject) => {
-            setTimeout (() => {
-                resolve (MOCK_DATA)
-        
-            },3000)
-    })
-
-}
-
-    pedirDatos()
+    useEffect (() => {
+        pedirDatos()
         .then ((data) => {
-            productos (data)
+            setProductos (data)
         })
+    }, [])
+    
 
 
     return (
         <section className="conteiner_item">
             <h1 className="title_item">{props}</h1>
             <hr/>
-            <p>{productos.length > 0 &&productos[0].name}</p>
+            
+            { productos.map((item) => (
+                <article>
+                    <img src={item.img} alt={item.name} />
+                    <h2>{item.name}</h2>
+                    <p>${item.precio}</p>
+                </article>
+            ))}
         </section>
     )
 } 
