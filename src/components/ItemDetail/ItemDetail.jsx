9fd0fbc62b7f2066ec329ef79a751c587b29f773
Ezequiel Cataneo <ezecataneo1991@gmail.com>
2023-12-19 18:12:27 -0300

    se aplico un poco mas de estilos

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,12 +2,12 @@
 export const ItemDetail = ({item}) => {
     
     return (
-        <div className="cards">
+        <div className="conteiner-render">
             <article className="card" key={item.id}>
                 <img className="imagen" src={item.img} alt={item.name} />
                 <h2>{item.name}</h2>
-                <p>${item.precio}</p>
-                <p>{item.description}</p>
+                <p className="precio-producto">${item.precio}</p>
+                <p className="descripcion">{item.description}</p>
                 <button className="btn-carrito">Agregar al carrito</button>
             </article>
         </div>
